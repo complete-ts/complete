e2d5b765780a923557bc69b314db5c3954052b8e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 13:03:06 -0400

    feat(node): checkCompiledOutputInRepo

--- packages/complete-node/src/functions/build.ts
+++ packages/complete-node/src/functions/build.ts
@@ -2,7 +2,7 @@
 
 import { assertDefined } from "complete-common";
 import fs from "node:fs";
-import { $s } from "./execa.js";
+import { $q, $s } from "./execa.js";
 import { getMatchingFilePaths, renameFileExtensions, rm } from "./file.js";
 
 /**
@@ -256,4 +256,25 @@ async function rewriteSourceMapFiles(
   }
 
   await Promise.all(writePromises);
+}
+
+/**
+ * Helper function to see if the compiled output that is checked-in to the Git repository is
+ * up-to-date.
+ *
+ * Note that compiled output (in e.g. the "dist" directory) is not usually committed to a Git
+ * repository, but this is necessary in certain cases, such as when writing a custom GitHub Action
+ * in TypeScript. In these situations, this function should be run as part of the linting stage in
+ * order to ensure that the compiled output is always up to date.
+ */
+export async function checkCompiledOutputInRepo(): Promise<void> {
+  await $q`npm run build`;
+
+  const gitStatusResult = await $q`git status --porcelain`;
+  const gitStatus = gitStatusResult.stdout;
+  const gitDirty = gitStatus === "";
+
+  if (gitDirty) {
+    process.exit(1);
+  }
 }
