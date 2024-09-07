4cddc86fd5f817473eea2bac0fec37b92edbb73c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-07 19:00:25 -0400

    fix: lint

--- packages/eslint-config-complete/src/monorepo.js
+++ packages/eslint-config-complete/src/monorepo.js
@@ -1,12 +1,17 @@
 import tseslint from "typescript-eslint";
 
 /**
- * This ESLint config is meant to be used in monorepos (in addition to the `completeBase` config).
+ * This ESLint config is meant to be used in monorepos that install dependencies at the root (in
+ * addition to the `completeBase` config).
  */
 export const completeConfigMonorepo = tseslint.config(
   {
     files: ["**/scripts/*.{js,cjs,mjs,ts,cts,mts}"],
-    rules: {},
+    rules: {
+      // The dependencies that are used in monorepo package scripts are often installed at the root
+      // of the monorepo (instead of as a "devDependency" in the package's "package.json" file).
+      "import-x/no-extraneous-dependencies": "off",
+    },
   },
 
   {
@@ -19,6 +24,10 @@ export const completeConfigMonorepo = tseslint.config(
       // ESLint configs in monorepos often intentionally import from the "packages" subdirectory
       // (because the config files are JavaScript so they cannot use tsconfig-paths).
       "import-x/no-relative-packages": "off",
+
+      // ESLint configs in monorepos will import from "typescript-eslint" (and potentially other
+      // plugins), which is often installed at root of the monorepo.
+      "import-x/no-extraneous-dependencies": "off",
     },
   },
 );
