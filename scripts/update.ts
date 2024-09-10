6a3cda089bba7befcc401267448699d705586f65
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-10 11:14:55 -0400

    chore: fix TS version

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,3 +1,7 @@
+// Packages held back:
+// - "typescript" - Stuck on "5.5.3" until TSESLint upgrades:
+// https://github.com/typescript-eslint/typescript-eslint/issues/9653
+
 import { updatePackageJSONDependenciesMonorepo } from "complete-node";
 
 await updatePackageJSONDependenciesMonorepo();
