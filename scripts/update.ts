2367b16e51c32829dddf842d8865dfbfbc6eb628
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-10-22 13:44:53 -0400

    chore: revert deps

--- scripts/update.ts
+++ scripts/update.ts
@@ -2,6 +2,11 @@
 // - "typescript" - Stuck on "5.5.4" until TSESLint upgrades:
 // https://github.com/typescript-eslint/typescript-eslint/issues/9653
 
-import { updatePackageJSONDependenciesMonorepo } from "complete-node";
+import { updatePackageJSONDependenciesMonorepoChildren } from "complete-node";
+import path from "node:path";
 
-await updatePackageJSONDependenciesMonorepo();
+/// await updatePackageJSONDependenciesMonorepo();
+
+const REPO_ROOT = path.join(import.meta.dirname, "..");
+
+await updatePackageJSONDependenciesMonorepoChildren(REPO_ROOT);
