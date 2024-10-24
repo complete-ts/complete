4a6c3dad156e0a29c1537f2863fccc1b5154bf6d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-10-24 19:48:45 -0400

    fix(node): declaration maps

--- packages/complete-node/scripts/build.ts
+++ packages/complete-node/scripts/build.ts
@@ -1,21 +1,82 @@
 import {
   $s,
   buildScript,
+  copyFileOrDirectory,
   deleteFileOrDirectory,
-  moveAllFilesInDirectory,
+  fixMonorepoPackageDistDirectory,
+  getMatchingFilePaths,
+  readFileAsync,
+  writeFileAsync,
 } from "complete-node";
+import os from "node:os";
 import path from "node:path";
 
-await buildScript((packageRoot) => {
-  $s`unbuild`; // We use the `unbuild` library to output both ESM and CJS.
+const OUTPUT_FILES = ["index.cjs", "index.mjs"] as const;
 
-  // `unbuild` does not generate declaration maps, so we must use the `tsc` to generate that.
-  $s`tsc --emitDeclarationOnly`;
-  const distDir = path.join(packageRoot, "dist");
-  const commonDir = path.join(distDir, "complete-common");
-  deleteFileOrDirectory(commonDir);
-  const completeNodeDir = path.join(distDir, "complete-node");
-  const srcDir = path.join(completeNodeDir, "src");
-  moveAllFilesInDirectory(srcDir, distDir);
-  deleteFileOrDirectory(completeNodeDir);
+await buildScript(async (packageRoot) => {
+  $s`unbuild`; // We use the `unbuild` library to output both ESM and CJS.
+  await buildDeclarationMaps(packageRoot);
 });
+
+/**
+ * `unbuild` does not generate declaration maps, so we must use `tsc` to generate that. However,
+ * first we move the unbuild output to a temporary directory so that it does not get overwritten.
+ * (It is not possible to configure `unbuild` to disable declarations, since reading the
+ * configuration file is bugged.)
+ */
+async function buildDeclarationMaps(packageRoot: string) {
+  const outDir = path.join(packageRoot, "dist");
+  const tmpDir = os.tmpdir();
+
+  for (const fileName of OUTPUT_FILES) {
+    const srcPath = path.join(outDir, fileName);
+    const dstPath = path.join(tmpDir, fileName);
+    copyFileOrDirectory(srcPath, dstPath);
+    deleteFileOrDirectory(srcPath);
+  }
+
+  deleteFileOrDirectory(outDir);
+  $s`tsc --emitDeclarationOnly`;
+  fixMonorepoPackageDistDirectory(packageRoot);
+  await fixDeclarationMaps(outDir);
+
+  for (const fileName of OUTPUT_FILES) {
+    const srcPath = path.join(tmpDir, fileName);
+    const dstPath = path.join(outDir, fileName);
+    copyFileOrDirectory(srcPath, dstPath);
+    deleteFileOrDirectory(srcPath);
+  }
+}
+
+/**
+ * After moving the declaration map files to a different directory, the relative path to the "src"
+ * directory will be broken.
+ *
+ * For example:
+ *
+ * ```json
+ * {"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../../../src/index.ts"]
+ * ```
+ *
+ * Needs to be rewritten to:
+ *
+ * ```json
+ * {"version":3,"file":"index.d.ts","sourceRoot":"","sources":["../src/index.ts"]
+ * ```
+ */
+async function fixDeclarationMaps(directoryPath: string): Promise<void> {
+  const extension = ".d.ts.map";
+  const matchFunc = (filePath: string) => filePath.endsWith(extension);
+  const filePaths = await getMatchingFilePaths(directoryPath, matchFunc);
+  const filesContents = await Promise.all(
+    filePaths.map(async (filePath) => readFileAsync(filePath)),
+  );
+  await Promise.all(
+    filePaths.map(async (filePath, i) => {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const fileContents = filesContents[i]!;
+      const newFileContents = fileContents.replaceAll("../../src/", "src/");
+      return writeFileAsync(filePath, newFileContents);
+    }),
+  );
+}
