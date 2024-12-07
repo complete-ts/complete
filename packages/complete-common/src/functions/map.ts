9cf3ad0cbd504d81c1a22933c1a77451939a05b9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-12-07 07:09:46 -0500

    chore: fix comment

--- packages/complete-common/src/functions/map.ts
+++ packages/complete-common/src/functions/map.ts
@@ -85,8 +85,8 @@ export function objectToMap<K extends string | number | symbol, V>(
  * undefined. In this situation, by converting the object to a map, you can avoid unsafe type
  * assertions.
  *
- * Note that the map values will be inserted in a random order, due to how `pairs` works under the
- * hood.
+ * Note that the converted map will only have string keys (due to the nature of JavaScript objects
+ * only having string keys under the hood).
  *
  * Also see the `objectToMap` function.
  */
