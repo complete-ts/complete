65427c260e215243bf8b3525daa11e12e772070a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-12-07 07:19:09 -0500

    fix: lint

--- packages/eslint-config-complete/scripts/extract-comments.d.ts
+++ packages/eslint-config-complete/scripts/extract-comments.d.ts
@@ -5,5 +5,5 @@ interface BlockComment {
 
 declare module "extract-comments" {
   const extractComments: (text: string) => BlockComment[];
-  export default extractComments;
+  export default extractComments; // eslint-disable-line import-x/no-default-export
 }
