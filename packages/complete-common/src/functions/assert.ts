9c84d526183783b0a8f3ab1109ea2a82e3604355
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-02-21 02:08:13 +0000

    feat(common): new type functions

--- packages/complete-common/src/functions/assert.ts
+++ packages/complete-common/src/functions/assert.ts
@@ -4,6 +4,21 @@
  * @module
  */
 
+import { isObject } from "./types.js";
+
+const TYPES_TO_VALIDATION_FUNCTIONS = {
+  string: (val: unknown): val is string => typeof val === "string",
+  number: (val: unknown): val is number => typeof val === "number",
+  boolean: (val: unknown): val is boolean => typeof val === "boolean",
+  object: (val: unknown): val is object =>
+    val !== null && typeof val === "object",
+  array: (val: unknown): val is unknown[] => Array.isArray(val),
+  null: (val: unknown): val is null => val === null,
+  undefined: (val: unknown): val is undefined => val === undefined,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  function: (val: unknown): val is Function => typeof val === "function",
+} as const;
+
 /**
  * Helper function to throw an error if the provided value is equal to `undefined`.
  *
@@ -36,6 +51,36 @@ export function assertNotNull<T>(
       ]
 ): asserts value is Exclude<T, null> {
   if (value === null) {
+    throw new TypeError(msg);
+  }
+}
+
+/**
+ * Helper function to throw an error if the provided value is not an object (i.e. a TypeScript
+ * record).
+ *
+ * This is useful to have TypeScript narrow a `Record<string, unknown> | undefined` value to
+ * `Record<string, unknown>` in a concise way.
+ *
+ * Under the hood, this function uses the `isObject` helper function.
+ */
+export function assertObject(
+  value: unknown,
+  msg: string,
+): asserts value is Record<string, unknown> {
+  if (!isObject(value)) {
+    throw new TypeError(msg);
+  }
+}
+
+/** Helper function to throw an error if the provided value is not of the provided type. */
+export function assertType(
+  value: unknown,
+  type: keyof typeof TYPES_TO_VALIDATION_FUNCTIONS,
+  msg: string,
+): asserts value is unknown {
+  const validationFunction = TYPES_TO_VALIDATION_FUNCTIONS[type];
+  if (!validationFunction(value)) {
     throw new TypeError(msg);
   }
 }
