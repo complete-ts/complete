ef73d1c739ff55361ca8550b62d161fd61244fd9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-03-02 08:07:39 -0500

    fix(docs): complete-cli on sidebar

--- packages/docs/sidebars.ts
+++ packages/docs/sidebars.ts
@@ -3,6 +3,7 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebars: SidebarsConfig = {
   docs: [
     "overview",
+    "complete-cli",
     {
       type: "category",
       label: "complete-common",
