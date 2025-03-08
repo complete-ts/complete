47ecb23f0143c19b5591701eae967edfa96b0646
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-03-08 12:15:20 -0500

    fix: docs building

--- packages/complete-node/typedoc.config.mjs
+++ packages/complete-node/typedoc.config.mjs
@@ -5,4 +5,14 @@ const config = getTypeDocConfig(import.meta.dirname);
 /** @type {import("typedoc").TypeDocOptions} */
 export default {
   ...config,
+
+  // TypeDoc complains about the re-exported dollar sign function from "execa".
+  intentionallyNotDocumented: [
+    "functions/execa.$.__type.stdout",
+    "functions/execa.$.__type.stderr",
+    "functions/execa.$.sync.__type.stdout",
+    "functions/execa.$.sync.__type.stderr",
+    "functions/execa.$.s.__type.stdout",
+    "functions/execa.$.s.__type.stderr",
+  ],
 };
