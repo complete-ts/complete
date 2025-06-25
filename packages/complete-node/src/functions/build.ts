d374437df0302ffa3f78275dcadcbdd456bdd44a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-06-25 13:54:04 -0400

    update

--- packages/complete-node/src/functions/build.ts
+++ packages/complete-node/src/functions/build.ts
@@ -4,7 +4,10 @@
  * @module
  */
 
-import { $o, $q } from "./execa.js";
+import path from "node:path";
+import { $, $o, $q } from "./execa.js";
+import { getPackageJSON } from "./packageJSON.js";
+import { getPackageRoot } from "./project.js";
 
 /**
  * Helper function to see if the compiled output that is checked-in to the Git repository is
@@ -24,4 +27,34 @@ export async function checkCompiledOutputInRepo(): Promise<void> {
   if (gitDirty) {
     process.exit(1);
   }
+}
+
+/**
+ * Helper function to compile a TypeScript project to a single file using Bun.
+ *
+ * Handily, this will automatically bake in the `APP_NAME` and `APP_VERSION` environment variables
+ * from the corresponding "package.json" file so that they can be used by the resulting program.
+ *
+ * @see https://bun.sh/docs/bundler/executables
+ */
+export async function compileToSingleFileWithBun(): Promise<void> {
+  const projectRoot = await getPackageRoot(2);
+  const packageJSONPath = path.join(projectRoot, "package.json");
+  const packageJSON = await getPackageJSON(packageJSONPath);
+  const { name, version } = packageJSON;
+
+  if (typeof name !== "string" || name === "") {
+    throw new Error(
+      `Failed to find the "name" field in the following file: ${packageJSONPath}`,
+    );
+  }
+
+  if (typeof version !== "string" || version === "") {
+    throw new Error(
+      `Failed to find the "version" field in the following file: ${packageJSONPath}`,
+    );
+  }
+
+  // We invoke Bun with `execa` instead of the API to avoid this package depending on "@types/bun".
+  await $`bun build --compile --target=bun-linux-x64 --minify --sourcemap ./src/main.ts --outfile ${name} --define:process.env.APP_VERSION=${version}`;
 }
