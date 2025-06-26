b88e523c2ec4ba769b9b0f0562025f633dd1c159
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-06-25 20:39:36 -0400

    chore: revert import-x dep

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,5 +1,3 @@
-// TODO: https://github.com/un-ts/eslint-plugin-import-x/issues/397
-// eslint-disable-next-line import-x/no-cycle
 import { updatePackageJSONDependenciesMonorepo } from "complete-node";
 
 await updatePackageJSONDependenciesMonorepo();
