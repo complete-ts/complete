a916d98b6e4af8f12959fce897330f0c563e448d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-08-04 14:29:46 -0400

    chore: remove useless test

--- packages/complete-node/src/functions/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { equal } from "node:assert";
-import test, { describe } from "node:test";
-import { isMain } from "./utils.js";
-
-describe("isMain", () => {
-  test("inside a test script", () => {
-    equal(isMain(), true);
-  });
-});
