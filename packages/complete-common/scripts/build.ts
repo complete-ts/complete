cd4a9475cea5fd6b8d8b2a962e660c9cf5260853
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-09-09 11:26:47 -0400

    update

--- packages/complete-common/scripts/build.ts
+++ packages/complete-common/scripts/build.ts
@@ -1,6 +1,6 @@
 import { buildScript } from "complete-node";
 import { unbuild } from "./unbuild.js";
 
-await buildScript(async (packageRoot) => {
+await buildScript(import.meta.dirname, async (packageRoot) => {
   await unbuild(packageRoot);
 });
