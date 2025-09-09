ce23b8d597560376b9748b99b5ad848918cdc5d7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-09-09 07:59:37 -0400

    feat(cli): source map support

--- packages/complete-cli/src/main.ts
+++ packages/complete-cli/src/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { Builtins, Cli } from "clipanion";
+import sourceMapSupport from "source-map-support";
 import { CheckCommand } from "./commands/CheckCommand.js";
 import { InitCommand } from "./commands/InitCommand.js";
 import { MetadataCommand } from "./commands/MetadataCommand.js";
@@ -12,6 +13,10 @@ import { PROJECT_NAME, PROJECT_VERSION } from "./constants.js";
 await main();
 
 async function main() {
+  // First, enable source map support so that if run-time errors occur, we can see the line of
+  // TypeScript code instead of the line of compiled JavaScript code.
+  sourceMapSupport.install();
+
   const [_node, _app, ...args] = process.argv;
 
   const cli = new Cli({
