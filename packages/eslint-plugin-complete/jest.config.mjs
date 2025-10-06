bee089dfed8171080dcfef9aa6239515adc2023d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-06 16:19:29 -0400

    update

--- packages/eslint-plugin-complete/jest.config.mjs
+++ packages/eslint-plugin-complete/jest.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 
-export default {
+/** @type {import("jest").Config} */
+const config = {
+  // See: https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring
   // Even though we are not using `ts-jest`, we must apply the same ESM fix as documented here:
   // https://github.com/swc-project/jest/issues/64#issuecomment-1029753225
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+
+  // See: https://jestjs.io/docs/configuration#testpathignorepatterns-arraystring
+  testPathIgnorePatterns: ["/dist/", "/node_modules/"],
 };
+
+export default config;
