8fc33b6dd6518192e119c6153b9ee549152d9ddb
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-06 15:21:38 -0400

    fix package lock

--- packages/eslint-plugin-complete/eslint-doc-generator.config.mjs
+++ packages/eslint-plugin-complete/eslint-doc-generator.config.mjs
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 /** @type {import("eslint-doc-generator").GenerateOptions} */
 const config = {
   // Defaults to "desc-parens-prefix-name", but we want to match the format established by ESLint
@@ -7,22 +9,32 @@ const config = {
   // Defaults to "[deprecated, configs, fixableAndHasSuggestions, requiresTypeChecking]".
   ruleDocNotices: ["description"],
 
-  postprocess(content, path) {
-    if (!path.includes("rules/") || !path.endsWith(".md")) {
+  ruleDocSectionExclude: [],
+
+  // Add a "Resources" section as the final section.
+  postprocess(content, pathToFile) {
+    if (
+      !pathToFile.includes(`${path.sep}rules${path.sep}`)
+      || !pathToFile.endsWith(".md")
+    ) {
       return content;
     }
 
-    const ruleName = path.split("/").pop().replace(".md", "");
+    const ruleName = pathToFile.split(path.sep).pop().replace(".md", "");
 
-    const resourcesSection = `
-## Resources
+    const resourcesSection = `## Resources
 
 - [How to use this rule](https://complete-ts.github.io/eslint-plugin-complete)
 - [Rule source](https://github.com/complete-ts/complete/blob/main/packages/eslint-plugin-complete/src/rules/${ruleName}.ts)
 - [Test source](https://github.com/complete-ts/complete/blob/main/packages/eslint-plugin-complete/tests/rules/${ruleName}.test.ts)
 `;
 
-    return content + resourcesSection;
+    const resourcesIndex = content.indexOf("## Resources");
+    if (resourcesIndex !== -1) {
+      content = content.slice(0, resourcesIndex);
+    }
+
+    return `${content.trim()}\n\n${resourcesSection}`;
   },
 };
 
