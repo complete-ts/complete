2f260b1e0449d6423430d0a7ca27063b54d8f450
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-06 15:44:32 -0400

    fix lint

--- packages/eslint-plugin-complete/eslint-doc-generator.config.mjs
+++ packages/eslint-plugin-complete/eslint-doc-generator.config.mjs
@@ -2,40 +2,53 @@ import path from "node:path";
 
 /** @type {import("eslint-doc-generator").GenerateOptions} */
 const config = {
-  // Defaults to "desc-parens-prefix-name", but we want to match the format established by ESLint
-  // and TSESLint: https://github.com/bmish/eslint-doc-generator/issues/347
-  ruleDocTitleFormat: "name",
+  // Defaults to "README.md".
+  pathRuleList: "website-root.md",
 
   // Defaults to "[deprecated, configs, fixableAndHasSuggestions, requiresTypeChecking]".
   ruleDocNotices: ["description"],
 
-  ruleDocSectionExclude: [],
+  // Defaults to true. See: https://github.com/bmish/eslint-doc-generator/issues/806
+  ruleDocSectionOptions: false,
 
-  // Add a "Resources" section as the final section.
+  // Defaults to "desc-parens-prefix-name", but we want to match the format established by ESLint
+  // and TSESLint: https://github.com/bmish/eslint-doc-generator/issues/347
+  ruleDocTitleFormat: "name",
+
   postprocess(content, pathToFile) {
-    if (
-      !pathToFile.includes(`${path.sep}rules${path.sep}`)
-      || !pathToFile.endsWith(".md")
-    ) {
-      return content;
-    }
+    return getContentWithResourcesSection(content, pathToFile);
+  },
+};
 
-    const ruleName = pathToFile.split(path.sep).pop().replace(".md", "");
+export default config;
 
-    const resourcesSection = `## Resources
+/**
+ * Add a "Resources" section as the final section.
+ *
+ * @param content {string}
+ * @param pathToFile {string}
+ */
+function getContentWithResourcesSection(content, pathToFile) {
+  if (
+    !pathToFile.includes(`${path.sep}rules${path.sep}`)
+    || !pathToFile.endsWith(".md")
+  ) {
+    return content;
+  }
+
+  const ruleName = pathToFile.split(path.sep).pop().replace(".md", "");
+
+  const resourcesSection = `## Resources
 
 - [How to use this rule](https://complete-ts.github.io/eslint-plugin-complete)
 - [Rule source](https://github.com/complete-ts/complete/blob/main/packages/eslint-plugin-complete/src/rules/${ruleName}.ts)
 - [Test source](https://github.com/complete-ts/complete/blob/main/packages/eslint-plugin-complete/tests/rules/${ruleName}.test.ts)
 `;
 
-    const resourcesIndex = content.indexOf("## Resources");
-    if (resourcesIndex !== -1) {
-      content = content.slice(0, resourcesIndex);
-    }
+  const resourcesIndex = content.indexOf("## Resources");
+  if (resourcesIndex !== -1) {
+    content = content.slice(0, resourcesIndex);
+  }
 
-    return `${content.trim()}\n\n${resourcesSection}`;
-  },
-};
-
-export default config;
+  return `${content.trim()}\n\n${resourcesSection}`;
+}
