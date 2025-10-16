cf8237dae1a6397b4c3ef65e472b8b9f86245b14
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-10-16 00:13:58 -0400

    fix: lint

--- packages/docs/eslint.config.mjs
+++ packages/docs/eslint.config.mjs
@@ -4,6 +4,19 @@ import { completeConfigBase } from "../eslint-config-complete/src/base.js";
 export default defineConfig(
   ...completeConfigBase,
 
+  // We must reset the upstream "allowDefaultProject" setting because it includes a glob pattern
+  // that matches "docusaurus.config.ts" and we need that file to be explicitly included in this
+  // project for type checking to work properly.
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
+      },
+    },
+  },
+
   {
     rules: {
       "import-x/no-default-export": "off", // React uses default exports.
