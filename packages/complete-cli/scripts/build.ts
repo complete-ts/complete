aced64f2e2a3d0af5a0c7815bd7bc1107292a1e0
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-04-18 13:59:13 -0400

    fix(cli): file-templates not existing

--- packages/complete-cli/scripts/build.ts
+++ packages/complete-cli/scripts/build.ts
@@ -1,8 +1,18 @@
-import { $, buildScript, fixMonorepoPackageDistDirectory } from "complete-node";
+import {
+  $,
+  buildScript,
+  copyFileOrDirectory,
+  fixMonorepoPackageDistDirectory,
+} from "complete-node";
+import path from "node:path";
 
 await buildScript(import.meta.dirname, async (packageRoot) => {
   await $`tsc`;
   await fixMonorepoPackageDistDirectory(packageRoot);
+
+  const fileTemplatesSrc = path.join(packageRoot, "file-templates");
+  const fileTemplatesDst = path.join(packageRoot, "dist", "file-templates");
+  await copyFileOrDirectory(fileTemplatesSrc, fileTemplatesDst);
 
   // If rapid-testing is needed during development, we can pack the dependencies using webpack to
   // avoid having to bump the versions of the other monorepo dependencies.
